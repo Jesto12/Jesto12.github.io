3afd1d5444e20b0ddac8dbac0a671fd834ecf312
Jesto13 <143734751+Jesto12@users.noreply.github.com>
2024-05-23 17:20:09 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('JavaScript cargado correctamente');
+    document.querySelectorAll('nav ul li a').forEach(anchor => {
+        anchor.addEventListener('click', function(e) {
+            e.preventDefault();
+            const targetId = this.getAttribute('href').substring(1);
+            document.getElementById(targetId).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
